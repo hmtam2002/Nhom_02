0fb9595608a32ff74950dc2b6cd29cd204d61992
tuongvycute <0306201285@caothang.edu.vn>
2022-12-11 21:44:39 +0700

    .

---
+++ Nhom_02/Areas/Admin/Controllers/ImagesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Nhom_02.Data;
+using Nhom_02.Models;
+
+namespace Nhom_02.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ImagesController : Controller
+    {
+        private readonly Nhom2Context _context;
+
+        public ImagesController(Nhom2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Images
+        public async Task<IActionResult> Index()
+        {
+            var nhom2Context = _context.Images.Include(i => i.Color);
+            return View(await nhom2Context.ToListAsync());
+        }
+
+        // GET: Admin/Images/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Images == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .Include(i => i.Color)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        // GET: Admin/Images/Create
+        public IActionResult Create()
+        {
+            ViewData["ColorId"] = new SelectList(_context.Colors, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/Images/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ColorId,Name")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(image);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ColorId"] = new SelectList(_context.Colors, "Id", "Id", image.ColorId);
+            return View(image);
+        }
+
+        // GET: Admin/Images/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Images == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images.FindAsync(id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+            ViewData["ColorId"] = new SelectList(_context.Colors, "Id", "Id", image.ColorId);
+            return View(image);
+        }
+
+        // POST: Admin/Images/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ColorId,Name")] Image image)
+        {
+            if (id != image.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(image);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImageExists(image.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ColorId"] = new SelectList(_context.Colors, "Id", "Id", image.ColorId);
+            return View(image);
+        }
+
+        // GET: Admin/Images/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Images == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .Include(i => i.Color)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        // POST: Admin/Images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Images == null)
+            {
+                return Problem("Entity set 'Nhom2Context.Images'  is null.");
+            }
+            var image = await _context.Images.FindAsync(id);
+            if (image != null)
+            {
+                _context.Images.Remove(image);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImageExists(int id)
+        {
+          return _context.Images.Any(e => e.Id == id);
+        }
+    }
+}
--- Nhom_02/Program.cs
+++ Nhom_02/Program.cs
@@ -22,17 +22,26 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
       name: "areas",
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
     );
-});
 
-
-app.MapControllerRoute(
+    app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
